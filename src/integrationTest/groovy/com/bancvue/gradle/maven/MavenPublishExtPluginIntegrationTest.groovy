6efe89e334788ada2d996d734ec51ed2e6f70a13
mike.lueders <mike.lueders@bancvue.com>
2013-09-06 18:10:04 -0500

    added maven publish plugin integration test

---
+++ src/integrationTest/groovy/com/bancvue/gradle/maven/MavenPublishExtPluginIntegrationTest.groovy
@@ -0,0 +1,32 @@
+package com.bancvue.gradle.maven
+
+import com.bancvue.gradle.test.AbstractPluginIntegrationTest
+import com.bancvue.gradle.test.TestFile
+import org.junit.Test
+
+
+class MavenPublishExtPluginIntegrationTest extends AbstractPluginIntegrationTest {
+
+	@Test
+	void shouldPublishArtifactAndSources() {
+		projectFS.file("src/main/java/Class.java") << "class Class {}"
+		TestFile mavenRepo = projectFS.mkdir("build/maven-repo")
+		projectFS.buildFile() << """
+ext.repositoryReleaseUrl='${mavenRepo.toURI()}'
+ext.artifactId='artifact'
+
+apply plugin: 'maven-publish-ext'
+
+group = 'group'
+version = '1.0'
+		"""
+
+		run("publishRemote")
+
+		assert projectFS.file("build/libs/artifact-1.0.jar").exists()
+		assert projectFS.file("build/libs/artifact-1.0-sources.jar").exists()
+		assert mavenRepo.file("group/artifact/1.0/artifact-1.0.jar").exists()
+		assert mavenRepo.file("group/artifact/1.0/artifact-1.0-sources.jar").exists()
+	}
+
+}
