529455782e28406c4f4eb34341303c63ab666c38
Mike Lueders <mike@roadkill.org>
2014-02-27 10:56:14 -0600

    DELTA-1403: propagate java.io.tmpdir to VMs which are instantiated for test and compilation

--- src/main/groovy/com/bancvue/gradle/ProjectDefaultsPlugin.groovy
+++ src/main/groovy/com/bancvue/gradle/ProjectDefaultsPlugin.groovy
@@ -19,9 +19,8 @@ import com.bancvue.gradle.categories.ProjectCategory
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.bundling.Jar
-import org.gradle.api.tasks.compile.ForkOptions
+import org.gradle.api.tasks.compile.BaseForkOptions
 import org.gradle.api.tasks.compile.GroovyCompile
-import org.gradle.api.tasks.compile.GroovyForkOptions
 import org.gradle.api.tasks.compile.JavaCompile
 import org.gradle.api.tasks.testing.Test
 
@@ -38,6 +37,7 @@ class ProjectDefaultsPlugin implements Plugin<Project> {
 		project.apply(plugin: 'java')
 		setDefaultCompileMemorySettings()
 		setDefaultTestMemorySettings()
+		propagateJavaTmpDir()
 		setCompilerEncoding()
 		setJavaCompatibilityVersion()
 		addBuildDateAndJdkToJarManifest()
@@ -46,16 +46,14 @@ class ProjectDefaultsPlugin implements Plugin<Project> {
 
 	private void setDefaultCompileMemorySettings() {
 		project.tasks.withType(GroovyCompile) { GroovyCompile compile ->
-			GroovyForkOptions forkOptions = compile.groovyOptions.forkOptions
-			setCompileMemorySettings(forkOptions)
+			setCompileMemorySettings(compile.groovyOptions.forkOptions)
 		}
 		project.tasks.withType(JavaCompile) { JavaCompile compile ->
-			ForkOptions forkOptions = compile.options.forkOptions
-			setCompileMemorySettings(forkOptions)
+			setCompileMemorySettings(compile.options.forkOptions)
 		}
 	}
 
-	private void setCompileMemorySettings(def forkOptions) {
+	private void setCompileMemorySettings(BaseForkOptions forkOptions) {
 		forkOptions.memoryInitialSize = defaultsProperties.minHeapSize
 		forkOptions.memoryMaximumSize = defaultsProperties.maxHeapSize
 		forkOptions.jvmArgs << "-XX:MaxPermSize=${defaultsProperties.maxPermSize}".toString()
@@ -66,6 +64,24 @@ class ProjectDefaultsPlugin implements Plugin<Project> {
 			test.minHeapSize = defaultsProperties.minTestHeapSize
 			test.maxHeapSize = defaultsProperties.maxTestHeapSize
 			test.jvmArgs("-XX:MaxPermSize=${defaultsProperties.maxTestPermSize}".toString())
+		}
+	}
+
+	private void propagateJavaTmpDir() {
+		String javaTmpDir = System.getProperty("java.io.tmpdir")
+		if (javaTmpDir) {
+			String jvmArgTmpDir = "-Djava.io.tmpdir=${javaTmpDir}".toString()
+
+			project.tasks.withType(Test) { Test test ->
+				test.jvmArgs(jvmArgTmpDir)
+			}
+			project.tasks.withType(GroovyCompile) { GroovyCompile compile ->
+				compile.groovyOptions.forkOptions.jvmArgs << jvmArgTmpDir
+			}
+			project.tasks.withType(JavaCompile) { JavaCompile compile ->
+				compile.options.forkOptions.jvmArgs << jvmArgTmpDir
+			}
+
 		}
 	}
 
